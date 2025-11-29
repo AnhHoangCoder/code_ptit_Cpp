fffb6b8a8abbf97099923393a50e45a6683b657c
May <hoangquocanh2n6@gmail.com>
2025-11-29 10:36:40 +0700

    PTIT25R1D - Xóa đi 1 kí tự

---
+++ OLP234.cpp
@@ -0,0 +1,25 @@
+// Cho xâu S có độ dài N. Kí hiệu Si là xâu S còn lại sau khi xóa bỏ kí tự thứ i.
+// Yêu cầu: Hãy đếm số cặp chỉ số (i, j) với 1 ≤ i < j ≤ N và Si = Sj.
+// Input: 
+// Dòng đầu tiên là số nguyên dương N (2 ≤ N ≤ 300000).
+// Dòng tiếp theo là xâu S gồm N kí tự.
+// Output:
+// In ra một số nguyên là số cặp chỉ số (i, j) thỏa mãn yêu cầu.
+// Test ví dụ:
+// Input:
+// 7
+// abbbcca
+// Output:
+// 4
+// Input:
+// 4
+// aaaa
+// Output:
+// 6
+// Input:
+// 2
+// ab
+// Output:
+// 0
+// Giải thích test 1: Các xâu thu được sau khi xóa bỏ 1 kí tự lần lượt là: bbbcca, abbcca, abbcca, abbcca, abbbca, abbbca, abbbcc.
+// Có 4 cặp chỉ số thỏa mãn là (2, 3), (2, 4), (3, 4) và (5, 6).
